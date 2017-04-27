30a2aec2fab2376f0d4574d698745419518963b6
Iron6ear <iron6ear@yandex.ru>
2017-04-27 09:01:08 +0300

    test

--- First_step_1/Lesson_7.py
+++ First_step_1/Lesson_7.py
@@ -1,1 +1,9 @@
 #Декомпозиция - разбиение целого на части
+temperature = []
+with open('temperature.txt', 'rt') as t:
+    for line in t:
+        temperature.append(int(line))
+
+avg = sum(temperature)/len(temperature)
+
+print('Средняя температура: {0}'.format(avg))
