70e911263473ed6b5995305861480a9bbc2ccd57
Iron6ear <iron6ear@yandex.ru>
2017-04-27 09:53:41 +0300

    test

--- First_step_1/Lesson_7.py
+++ First_step_1/Lesson_7.py
@@ -1,9 +1,34 @@
-#Декомпозиция - разбиение целого на части
+# Декомпозиция - разбиение целого на части
+# .strip() - удаление пробельных символов или других символов
+# .split() - разделение строки и привращение в список
+# .extend() - соединение списков
+
+from pprint import pprint
+
 temperature = []
-with open('temperature.txt', 'rt') as t:
+with open('temperature.txt') as t:
     for line in t:
         temperature.append(int(line))
 
 avg = sum(temperature)/len(temperature)
 
 print('Средняя температура: {0}'.format(avg))
+
+with open('average_temperature.txt', 'w') as average_temperature:
+    average_temperature.write(str(avg))
+
+cities = dict()
+
+with open('week_temperature.txt') as f_week_temperature:
+    for city in f_week_temperature:
+        temperatures = f_week_temperature.readline()
+        cities[city.strip()] = temperatures.split()
+
+pprint(cities)
+
+for city, temperatures in cities.items():
+    avg = 0
+    for t in temperatures:
+        avg += int(t)
+    avg = avg / len(temperatures)
+    print(city, '%.2f' % avg)
