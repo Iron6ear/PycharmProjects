e2512caa2e61f91901afe12ce907be68c7161a5f
Iron6ear <iron6ear@yandex.ru>
2017-05-15 15:46:46 +0300

    complit task

---
+++ Netology_2/Lesson_3.py
@@ -0,0 +1,11 @@
+import os.path
+import codecs
+
+local_path = os.path.dirname(os.path.realpath(__file__))
+countries_path = os.path.join(local_path, 'countries.txt')
+
+print(countries_path)
+
+with codecs.open(countries_path, encoding='UTF-8') as countries:
+    for country in countries:
+        print(country.strip())
